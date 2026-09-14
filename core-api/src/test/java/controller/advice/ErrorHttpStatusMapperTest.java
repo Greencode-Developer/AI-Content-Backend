6e63dab0edc06378ad927b752db1eeceb2b27b44
camdao <luongcamdao@gmail.com>
2026-09-14 21:55:30 +0700

    feat: add CustomException

---
+++ core-api/src/test/java/controller/advice/ErrorHttpStatusMapperTest.java
@@ -0,0 +1,16 @@
+package controller.advice;
+
+import com.ai_content.common.error.ErrorCode;
+import com.ai_content.controller.advice.ErrorHttpStatusMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ErrorHttpStatusMapperTest {
+    private final ErrorHttpStatusMapper errorHttpStatusMapper = new ErrorHttpStatusMapper();
+
+    @Test
+    void testMapErrorToHttpStatus() {
+        assertThat(errorHttpStatusMapper.toHttpStatus(ErrorCode.SAMPLE_ERROR)).isEqualTo(HttpStatus.BAD_REQUEST);
+    }
+}
