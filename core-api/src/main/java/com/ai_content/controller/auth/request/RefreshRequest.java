f93fd7286eb8627dc54b8951b5f58f644c53ce59
camdao <luongcamdao@gmail.com>
2026-09-16 19:30:10 +0700

    feat: check @Email

--- core-api/src/main/java/com/ai_content/controller/auth/request/RefreshRequest.java
+++ core-api/src/main/java/com/ai_content/controller/auth/request/RefreshRequest.java
@@ -1,9 +1,11 @@
 package com.ai_content.controller.auth.request;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public record RefreshRequest(
         @NotBlank
+        @Email
         String refreshToken
 ) {
 }
